467c1b7fc3c70d2b7ddd3d2af28a1d1a9a85cf98
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-06-22 14:45:13 +0530

    basic code

---
+++ code.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std ; 
+
+int main () {
+    int price ; 
+    // cout << "Enter the price:" ; 
+    // cin >> price ; 
+    // cout << "Dharmesh\nYadav:\n" ;
+    // cout << "Your entered price is: " << price << endl;
+    cout << (3>5) << endl ;
+    cout << (3/ (double)2) << endl ;
+    // cout << ()
+    //* sum of 2 numbers 
+    int num1, num2, sum ; 
+    cout << "Enter the number 1:" ;
+    cin >> num1 ; 
+    cout << "Enter the number 2:" ; 
+    cin >> num2 ; 
+    sum = num1 + num2 ;
+    cout << "Sum of two numbers is " << sum ; 
+    return 0; 
+}
