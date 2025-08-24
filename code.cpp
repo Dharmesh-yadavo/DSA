9a4b27682c8a05bde9d54b1cbdac2c1f895bb240
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-08-24 23:17:56 +0530

    basic stuff added

--- code.cpp
+++ code.cpp
@@ -1,5 +1,6 @@
 # include <iostream>
-using namespace std ; 
+using namespace std ;
+// if you want all libraries you can add using one header file: #include <bits/stdc++.h>  
 
 int main () {
     int price ; 
@@ -16,7 +17,34 @@ int main () {
     cin >> num1 ; 
     cout << "Enter the number 2:" ; 
     cin >> num2 ; 
+    // cin >> num1 >> num2 ;
+    // cout << "Enter the number num1 = " << num1 << " and num2 = " << num2 ; 
     sum = num1 + num2 ;
     cout << "Sum of two numbers is " << sum ; 
+    //! int : -10^9 to 10^9
+    //! long : -10^12 to 10^12
+    //! long long : -10^18 to 10^18
+    // long x = 15; 
+    // long long x = 1500000000;
+    //! float, double : 
+    // float x = 5.67;
+    // float y = 5; // 5
+    // doble y = 5;
+    //! string and getline : if you say string it only picks up letters before the space 
+    // string s;
+    // cin >> s; // Hey Dharmesh 
+    // cout << s; // Hey
+    //* for taking both input we have to make strings s1 and s2 ;
+    // ~ if you want to take all in string then you have to use getline . it only picks up one line 
+    // string str;
+    // getline(cin, str);  // Hey Dharmesh I am Cool 
+    // cout << str; // Hey Dharmesh I am Cool 
+    //! char : used to declare only a single character 
+    // char ch;
+    // cin >> ch; // h 
+    // cout << ch; // h
+
+
+
     return 0; 
 }
