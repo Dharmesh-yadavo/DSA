f76bb5b7a82d41e358d0c1afabb97709ea348896
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-08-25 20:29:08 +0530

    pair in C++

--- Patterns.cpp
+++ Patterns.cpp
@@ -4,9 +4,9 @@ using namespace std ;
 int main() {
 
     //! Square pattern 
-    int n; 
-    cout << "Please enter the number of rows: " ;
-    cin >> n ; 
+    // int n; 
+    // cout << "Please enter the number of rows: " ;
+    // cin >> n ; 
     // for(int i = 1 ; i <= n ; i++){
     //     for(int j = 1 ; j <= 4 ; j++){
     //         cout << j << " " ; 
@@ -167,19 +167,154 @@ D D D D */
 //     cout << endl ;
 // }
 
+//  g++ ConditionalStatementsAndLoops -o ConditionalStatementsAndLoops.exe     
 //! Pyramid pattern 
-for(int i = 0; i < n ; i++){
-    for(int j = 0; j < n-i-1; j++){
-        cout << " " ; 
-    }
-    for(int j = 1; j <= i+1 ; j++){
-        cout << j ; 
-    }
-    for(int j = i ; j > 0; j--){
-        cout << j ; 
-    }
-    cout << endl ;
-}
+// for(int i = 0; i < n ; i++){
+//     for(int j = 0; j < n-i-1; j++){
+//         cout << " " ; 
+//     }
+//     for(int j = 1; j <= i+1 ; j++){
+//         cout << j ; 
+//     }
+//     for(int j = i ; j > 0; j--){
+//         cout << j ; 
+//     }
+//     cout << endl ;
+// }
+
+//~ DSA Striver :::
+//! 1  
+// int n = 4; 
+// for(int i = 0; i < n; i++){
+//     for(int j = 0; j < n; j++){
+//         cout << "*" ; 
+//     }
+//     cout << endl ;
+// }
+
+//! 2 
+// for (int i = 0 ; i < 5; i++){
+//     for(int j = 0; j <= i; j++){
+//         cout << "*";
+//     }
+//     cout << endl ;
+// }
+
+//! 3 
+// for (int i = 1 ; i <= 5; i++){
+//     for(int j = 1; j <= i; j++){
+//         cout << j;
+//     }
+//     cout << endl ;
+// }
+
+//! 4
+// for (int i = 1 ; i <= 5; i++){
+//     for(int j = 1; j <= i; j++){
+//         cout << i;
+//     }
+//     cout << endl ;
+// }
+
+//! 5
+// for(int i = 0; i < 5; i++ ){
+//     for(int j = 5; j > i; j --){
+//         cout << "*";
+//     }
+//     cout << endl;
+// }
+
+//! 6 
+// int n = 5; 
+// for(int i = 0; i < n; i++ ){
+//     for(int j = 1; j < n - i + 1; j++){
+//         cout << j << " ";
+//     }
+//     cout << endl;
+// }
+
+//! 7
+// int n = 5;
+// for(int i = 0; i < n; i++){
+//     //space 
+//     for(int j = 0;j < n - i - 1; j++){
+//         cout << " ";
+//     }
+//     //star
+//     for(int j = 0; j < 2i + 1; j++){
+//         cout << "*";
+//     }
+//     //space
+//     for(int j = 0; j < n - i - 1; j++){
+//         cout << " ";
+//     }
+// }
+
+//! 8 
+// for(int i = 0; i<5; i++){
+//     for(int j=0; j < i; j++){
+//         cout << " ";
+//     }
+//     for(int j = 9 - 2*i; j > 0; j--){
+//         cout << "*";
+//     }
+//     for(int j=0; j < i; j++){
+//         cout << " ";
+//     }
+//     cout << endl;
+// }
+
+//! 9
+// 7 + 8
+
+//! 10 
+
+//! 11
+// for(int i = 0; i < 5; i++){
+//     int arr = [1, 0];
+//     for(int j = 0; j < i; j++){
+//         cout << arr[j]
+//     }
+// }
+
+//! 12 
+// int n = 4;
+// for (int i = 1; i <= n; i++){
+//     // number 
+//     for(int j = 1; j <= i; j++){
+//         cout << j;
+//     };
+//     // space 
+//     for(int j = 1; j <= 2 * ( n - i ); j++){
+//         cout << " ";
+//     }
+//     // number
+//     int start = i;
+//     for(int j = 1; j <= i; j++){
+//         cout << start;
+//         start = start - 1;
+//     };
+//     cout << endl;
+// }
+
+//! 13 
+// int n = 5;
+// int start  = 1; 
+// for(int i = 0; i < n; i++){
+//     start = start + i; 
+//     int res = start;
+//     for(int j = 0; j <= i; j++){
+//         cout << res << " ";
+//         res = res + 1 ; 
+//     }
+//     cout << endl;
+// }
+
+//! 14
+
+
+
+
 
     return 0 ; 
 }
