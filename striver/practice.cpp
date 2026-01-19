d09146e740d69ee3dd7e50df5ced2ef1c102ee8a
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-19 11:10:23 +0530

    hashing part practice done

--- striver/practice.cpp
+++ striver/practice.cpp
@@ -222,6 +222,115 @@ int nFibonacciNumber(int n) {
     return last + sLast  ; 
 }
 
+void hashArray() {
+    int arr[] = {1, 2, 1, 2, 3} ; 
+    int s = sizeof(arr) / sizeof(arr[0]);
+    // Brute force approach 
+    // int cnt = 0; 
+    // int q = 1;
+    // for(int i = 0; i < s; i++ ){
+    //     if(arr[i] == q){
+    //         cnt ++;
+    //     }
+    // }
+    // cout << cnt << endl; 
+    //~ Time Complexity: O(N²), Space Complexity: O(N)
+
+    //! optimal approach 
+    // int q[] = {1, 2, 3, 4}; 
+
+    int hash[6] = {0};
+    for(int i = 0; i < s; i++){
+        hash[arr[i]] += 1;
+    }
+    int q ;
+    cin >> q ;
+    while (q--) {
+        int number;
+        cin >> number;
+        // fetching:
+        cout << hash[number] << endl;
+    }
+    //~ Time Complexity: O(N), Space Complexity: O(N)
+}
+
+void characterHashing() {
+    string s = "dharmesh";
+
+    int hash[26] = {0}; 
+    for(int i = 0; i < s.size(); i++ ){
+        hash[s[i] - 'a'] += 1;
+    }
+
+    int q ;
+    cin >> q ;
+    while (q--) {
+       char c;
+        cin >> c;
+        // fetching:
+        cout << hash[c - 'a'] << endl;
+    }
+ 
+}
+
+void stringHashing(){
+    string s;
+    cin >> s;
+
+    //precompute:
+    int hash[256] = {0};
+    for (int i = 0; i < s.size(); i++) {
+        hash[s[i]]++;
+    }
+
+    int q;
+    cin >> q;
+    while (q--) {
+        char c;
+        cin >> c;
+        // fetch:
+        cout << hash[c] << endl;
+    }
+
+}
+
+void highestLowestFreq() {
+    int arr[] = {10,5,10,15,10,5};
+    int s = sizeof(arr) / sizeof(arr[0]);
+
+    unordered_map<int, int> map;
+
+    for(int i = 0; i < s; i++){
+        map[arr[i]] ++;
+    }
+    int maxFreq = 0, minFreq = s;
+    int maxEle = 0, minEle = 0;
+    for(auto it: map){
+        int element = it.first;
+        int count = it.second;
+
+        cout << element << " " << count << endl;
+
+        // Update max frequency element
+            if (count > maxFreq) {
+                maxFreq = count;
+                maxEle = element;
+            }
+
+            // Update min frequency element
+            if (count < minFreq) {
+                minFreq = count;
+                minEle = element;
+            }
+    }
+    
+    cout << "The highest frequency element is: " << maxEle << "\n";
+    cout << "The lowest frequency element is: " << minEle << "\n";
+   
+}
+
+
+
 
 
 
@@ -264,7 +373,17 @@ int main () {
     // optimal approach: 
     // checkPalindromeStringOptimalApproach(s); 
 
-    cout << nFibonacciNumber(4);
+    // cout << nFibonacciNumber(4);
+
+    //! hashing 
+
+    // hashArray();
+    
+    // characterHashing(); 
+    // string containing both uppercase and lowercase then 
+    // stringHashing();
+
+    highestLowestFreq();
 
 
     
